30823086591b96c9c8fbdad32a1aa222bc4b9403
MD Milon <milon@kali>
2025-01-08 20:49:02 +0600

    add some validation to prevent same socket user join multiple time

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
   cors: {
     origin: "*",
   },
 });
-const messageDB = [];
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+
+const chatDB = [];
+let activeUser = [];
+
+// Log active users for debugging
+console.log(activeUser);
+
 io.on("connection", (socket) => {
+  // Join room logic
   socket.on("joinRoom", (info) => {
-    socket.join(info.room);
-    io.to(info.room).emit("sendResponseEvent", messageDB);
+    const checkSocketID = activeUser.filter(
+      (user) => user.socket === socket.id
+    );
+
+    const checkUser = checkSocketID.some(
+      (user) => user.name.toLowerCase() === info.name.toLowerCase()
+    );
+
+    if (checkUser) {
+      console.log("User with this name already exists.");
+      return socket.emit("serverResponse", {
+        message: "User with this name already exists in the room",
+        type: false,
+      });
+    } else {
+      console.log("Joining room:", info.room);
+      socket.join(info.room);
+      const user = { ...info, socket: socket.id };
+      activeUser.unshift(user);
+
+      const roomData = chatDB.filter((msg) => msg.room === info.room);
+      const userData = activeUser.filter((user) => user.room === info.room);
+
+      io.to(info.room).emit("chatEvent", roomData);
+      io.to(info.room).emit("activeUserEvent", userData);
+    }
   });
-  socket.on("messageEvent", (payload) => {
-    messageDB.unshift(payload);
-    io.to(payload.room).emit("sendResponseEvent", messageDB);
+
+  // Handle chat events
+  socket.on("chatEvent", (data) => {
+    chatDB.unshift(data);
+    io.to(data.room).emit("chatEvent", chatDB);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    const userIndex = activeUser.findIndex((user) => user.socket === socket.id);
+
+    if (userIndex !== -1) {
+      const user = activeUser.splice(userIndex, 1)[0];
+      const userData = activeUser.filter((u) => u.room === user.room);
+      io.to(user.room).emit("activeUserEvent", userData);
+    }
   });
 });
 
+// Start server
 server.listen(4000, () => console.log("Server running on port 4000"));
