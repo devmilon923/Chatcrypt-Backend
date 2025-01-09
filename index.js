b874aa90b34c0f813cce91e13a0ec562428b922f
MD Milon <milon@kali>
2025-01-09 21:44:30 +0600

    add some validation to join a room

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+
 const socketIo = require("socket.io");
 
 const app = express();
@@ -13,49 +14,67 @@ const io = socketIo(server, {
 const chatDB = [];
 let activeUser = [];
 
-// Log active users for debugging
-console.log(activeUser);
-
 io.on("connection", (socket) => {
   // Join room logic
   socket.on("joinRoom", (info) => {
-    const checkSocketID = activeUser.filter(
-      (user) => user.socket === socket.id
+    const isNameTaken = activeUser.some(
+      (user) =>
+        user.room?.toLowerCase() === info.room?.toLowerCase() &&
+        user.name?.toLowerCase() === info.name?.toLowerCase()
     );
-
-    const checkUser = checkSocketID.some(
-      (user) => user.name.toLowerCase() === info.name.toLowerCase()
-    );
-
-    if (checkUser) {
-      console.log("User with this name already exists.");
-      return socket.emit("serverResponse", {
-        message: "User with this name already exists in the room",
+    if (isNameTaken) {
+      console.log(
+        `Username "${info.name}" is already taken in room "${info.room}".`
+      );
+      socket.emit("serverResponse", {
+        message: `User name must be unique in room`,
         type: false,
       });
-    } else {
-      console.log("Joining room:", info.room);
-      socket.join(info.room);
-      const user = { ...info, socket: socket.id };
-      activeUser.unshift(user);
-
-      const roomData = chatDB.filter((msg) => msg.room === info.room);
-      const userData = activeUser.filter((user) => user.room === info.room);
-
-      io.to(info.room).emit("chatEvent", roomData);
-      io.to(info.room).emit("activeUserEvent", userData);
+      return;
     }
+    // Check if the socket ID is already present in the specified room
+    const isSocketIDTaken = activeUser.some(
+      (user) =>
+        user.room?.toLowerCase() === info.room?.toLowerCase() &&
+        user.socket === socket.id
+    );
+
+    if (isSocketIDTaken) {
+      console.log(
+        `Socket ID "${socket.id}" is already associated with room "${info.room}".`
+      );
+      socket.emit("serverResponse", {
+        message: `You are already in`,
+        type: false,
+      });
+      return;
+    }
+
+    // console.log("Joining room:", info.room);
+    socket.join(info.room);
+    const user = { ...info, socket: socket.id };
+    activeUser.unshift(user);
+
+    const roomData = chatDB.filter((msg) => msg.room === info.room);
+    const userData = activeUser.filter((user) => user.room === info.room);
+
+    io.to(info.room).emit("chatEvent", roomData);
+    io.to(info.room).emit("activeUserEvent", userData);
+    socket.emit("serverResponse", {
+      message: `You are in`,
+      type: true,
+    });
+    return;
   });
 
   // Handle chat events
   socket.on("chatEvent", (data) => {
-    chatDB.unshift(data);
+    chatDB.push(data);
     io.to(data.room).emit("chatEvent", chatDB);
   });
 
   // Handle disconnection
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
     const userIndex = activeUser.findIndex((user) => user.socket === socket.id);
 
     if (userIndex !== -1) {
