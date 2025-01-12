a6ceb321d7b6583b89a646e85a263d458caab189
MD Milon <milon@kali>
2025-01-12 20:38:59 +0600

    change client url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "https://socket-group-chat.netlify.app",
+    origin: "https://chatcrypt.netlify.app",
     methods: ["GET", "POST"],
   },
 });
